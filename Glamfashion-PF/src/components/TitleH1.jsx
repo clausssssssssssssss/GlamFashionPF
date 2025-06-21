7d66aa2ab1c76d5e3938a6d28efce1c088feaae5
Allan <alaanrivas333@gmail.com>
2025-06-21 14:59:24 -0600

    Agrege los titulos 1 y 2

---
+++ Glamfashion-PF/src/components/TitleH1.jsx
@@ -0,0 +1,9 @@
+const TitleH1 = ({ text }) => {
+  return (
+    <h1 className="text-4xl font-bold bg-gradient-to-r from-gray-400 to-gray-900 bg-clip-text text-transparent p-4">
+      {text}
+    </h1>
+  );
+};
+
+export default TitleH1;
---
+++ Glamfashion-PF/src/components/TitleH2.jsx
@@ -0,0 +1,8 @@
+const TitleH2 = ({ label }) => {
+  return (
+    <h2 className="text-2xl font-bold text-gray-700 mb-2 text-center">
+      {label}
+    </h2>
+  );
+};
+export default TitleH2;
