188751f078d844de132901aa0a97513dae044a4d
Ruth <20200180@ricaldone.edu.sv>
2025-06-21 14:57:10 -0600

    AGREGE SpanText :)

---
+++ Glamfashion-PF/src/components/SpanText.jsx
@@ -0,0 +1,8 @@
+const SpanText = ({ text }) => {
+  return (
+    <div className="bg-amber-50 border border-amber-200 rounded-lg p-3 mb-6">
+      <p className="text-sm text-amber-800 flex items-center">{text}</p>
+    </div>
+  );
+};
+export default SpanText;
